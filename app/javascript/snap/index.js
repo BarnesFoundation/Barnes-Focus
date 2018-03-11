1e41e87716e26db2cdb319ced08d9f4240d35633
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-03-12 01:24:54 +0530

    Bootstrap carousel styles.

--- app/javascript/snap/index.js
+++ app/javascript/snap/index.js
@@ -6,6 +6,7 @@ import AppRouter from './routes/AppRouter';
 
 //import 'normalize.css/normalize.css';
 import 'bootstrap/scss/bootstrap.scss'
+import 'react-select/dist/react-select.css';
 import './styles/styles.scss';
 
 
