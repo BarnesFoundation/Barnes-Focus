64015c6bd87efc545dd7d53501c36887e70c1040
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-03-15 01:28:49 +0530

    Added all four screens for welcome screen carousel.

--- app/javascript/snap/components/Header.js
+++ app/javascript/snap/components/Header.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router'
 import barnes_logo from 'images/logo.svg';
 
-const Header = () => (
-    <nav className="narbar header">
-        <a className="navbar-brand" href="#" ui-sref="home.market">
-            <img src={barnes_logo} alt="Barnes" />
-        </a>
-    </nav>
-)
+/** 
+ * withHeader HOC provides props with location, history and match objects
+*/
+class Header extends Component {
 
-export default Header;
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.location.pathname !== '/snap' &&
+                    <nav className="narbar header">
+                        <a className="navbar-brand">
+                            <img src={barnes_logo} alt="Barnes" />
+                        </a>
+                    </nav>
+                }
+            </div>
+        );
+    }
+}
+
+export default withRouter(Header);
