d909df6b2af7c78ba8d5d7bb3e3f9e406a35fe42
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-03-18 02:01:51 +0530

    language select modal setup and store preference in localstorage.

--- app/javascript/snap/components/Welcome.js
+++ app/javascript/snap/components/Welcome.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Hammer from 'hammerjs';
-
 import { Link } from 'react-router-dom';
+
+import Hammer from 'hammerjs';
+import Modal from 'react-modal';
+
 import img1 from 'images/welcome-img1.jpg';
 import img2 from 'images/welcome-img2.jpg';
 import img3 from 'images/welcome-img3.jpg';
-
 import img4 from 'images/lang-img1.jpg';
 import img5 from 'images/lang-img2.jpg';
 import img6 from 'images/lang-img3.jpg';
-
 import img7 from 'images/wifi-img1.jpg';
 import img8 from 'images/wifi-img2.jpg';
 import img9 from 'images/wifi-img3.jpg';
-
 import photo_prompt from 'images/photo-prompt.jpg';
 import icon_camera from 'images/camera_icon.svg';
 
 class WelcomeComponent extends Component {
 
     state = {
-        selectedLanguage: 'English'
+        selectedLanguage: localStorage.getItem('barnes.snap.pref.lang') || 'English (Default)',
+        modalIsOpen: false,
+        languageOptions: [
+            'English (Default)',
+            '中文',
+            'Français',
+            'Deutsch',
+            'Italiano',
+            '日本語',
+            '한국어',
+            'русский',
+            'Español'
+        ]
     }
 
     componentDidMount() {
@@ -50,17 +61,26 @@ class WelcomeComponent extends Component {
         });
 
         $('.dropdown-toggle').dropdown();
+
+        Modal.setAppElement('#language-select');
     }
 
-    handleChange = (ev) => {
-        this.setState({ selectedLanguage: ev.target.value });
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    }
 
-        setTimeout(() => { console.log(`Selected: ` + this.state.selectedLanguage) })
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
+
+    selectLanguage = (e) => {
+        const selectedLanguage = e.currentTarget.dataset.id;
+        this.setState({ selectedLanguage: selectedLanguage });
+        localStorage.setItem('barnes.snap.pref.lang', selectedLanguage);
+        this.closeModal();
     }
 
     render() {
-        const { selectedLanguage } = this.state;
-        const lang = selectedLanguage && selectedLanguage.value;
         return (
             <div id="snapCarousel" className="carousel slide">
                 <ol className="carousel-indicators">
@@ -88,7 +108,6 @@ class WelcomeComponent extends Component {
                                 <h1>It's a snap!</h1>
 
                                 <p>Take a photo of any work of art to get information about it.</p>
-                                {/* <Link className="btn btn-primary" to="/snap">Continue</Link> */}
                             </div>
                         </div>
                     </div>
@@ -105,56 +124,38 @@ class WelcomeComponent extends Component {
                                     <img src={img6} alt="img6" />
                                 </div>
                             </div>
-                            <div className="content">
+                            <div id="language-select" className="language-select text-center">
 
-                                <h2>Please select your language</h2>
+                                <h1>Please select your language</h1>
 
-                                {/* <select className="custom-select">
-                                    <option value="English" selected>English (Default)</option>
-                                    <option value="Français">Français</option>
-                                    <option value="Deutsch">Deutsch</option>
-                                    <option value="Italiano">Italiano</option>
-                                    <option value="Español">Español</option>
-                                </select> */}
-
-                                {/* <Select
-                                    name="form-field-name"
-                                    value={lang}
-                                    onChange={this.handleChange}
-                                    options={[
-                                        { value: 'English', label: 'English (Default)' },
-                                        { value: 'Français', label: 'Français' },
-                                        { value: 'Deutsch', label: 'Deutsch' },
-                                        { value: 'Italiano', label: 'Italiano' },
-                                        { value: 'Español', label: 'Español' }
-
-                                    ]}
-                                /> */}
-
-                                <form onSubmit={this.handleSubmit}>
-                                    <label>
-
-                                        <select value={this.state.selectedLanguage} onChange={this.handleChange}>
-                                            <option value="English">English (Default)</option>
-                                            <option value="Français">Français</option>
-                                            <option value="Deutsch">Deutsch</option>
-                                            <option value="Italiano">Italiano</option>
-                                            <option value="Español">Español</option>
-                                        </select>
-                                    </label>
-                                </form>
-
-                                {/* <div className="dropdown">
-                                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        English (Default)
-  </button>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                        <a className="dropdown-item" href="#">Français</a>
-                                        <a className="dropdown-item" href="#">Deutsch</a>
-                                        <a className="dropdown-item" href="#">Italiano</a>
-                                        <a className="dropdown-item" href="#">Español</a>
+                                <div className="row">
+                                    <div className="col-12 col-md-4 offset-md-4">
+                                        <div className="btn-group d-flex" role="group">
+                                            <button className="btn btn-secondary btn-lg w-100" type="button" onClick={this.openModal}>
+                                                {this.state.selectedLanguage}
+                                            </button>
+                                            <button type="button" className="btn btn-lg btn-secondary dropdown-toggle dropdown-toggle-split" onClick={this.openModal}>
+                                                <span className="sr-only">Toggle Dropdown</span>
+                                            </button>
+                                        </div>
                                     </div>
-                                </div> */}
+                                </div>
+                                <Modal
+                                    isOpen={this.state.modalIsOpen}
+                                    onRequestClose={this.closeModal}
+                                    contentLabel="Example Modal"
+                                    className="Modal"
+                                    overlayClassName="Overlay"
+                                >
+                                    <button type="button" className="close pull-right offset-11" aria-label="Close" onClick={this.closeModal}>
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h1>Please select your language.</h1>
+                                    <p>We are using Google to help us automatically translate our text.</p>
+                                    <ul className="list-group">
+                                        {this.state.languageOptions.map(lang => <li className="list-group-item" key={lang} data-id={lang} onClick={this.selectLanguage}>{lang}</li>)}
+                                    </ul>
+                                </Modal>
                             </div>
                         </div>
                     </div>
@@ -206,7 +207,7 @@ class WelcomeComponent extends Component {
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </a>
-            </div>
+            </div >
         );
     }
 }
