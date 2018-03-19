6bd08ba53e94d04bb993aac86fd035111c81017a
Tarun Kumar <tarun.169@gmail.com>
2018-03-19 09:29:13 +0530

    moved image search to two thread

--- app/controllers/api/snaps_controller.rb
+++ app/controllers/api/snaps_controller.rb
@@ -15,10 +15,10 @@ class Api::SnapsController < Api::BaseController
       f.write image_data
     end
     # for testing
-    #file_name = "#{Rails.root}/public/test-image1.jpeg"
+    #file_name = "#{Rails.root}/public/IMG_4971.jpg"
 
     file = pastec_obj.loadFileData(file_name)
-    pastec_response = pastec_obj.search_image(file)
+    pastec_response = pastec_obj.search_image(file_name)
 
     pastec_response["image_ids"] = pastec_response["type"]== Pastec::RESPONSE_CODES[:SEARCH_RESULTS] ? [pastec_response["results"].collect{|k| File.basename(k.keys[0], ".jpg").split('_')[0]}.compact[0]].compact : []
 
