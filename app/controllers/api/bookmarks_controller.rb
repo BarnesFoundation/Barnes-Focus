82a786c25811ee5abcb2a7ffc8be265a4448bb08
Puneet Pandey <er.puneetpandey@gmail.com>
2018-03-29 14:04:51 +0530

    SV-30

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,10 +1,21 @@
-require 'barnes_elastic_search'
-
 class Api::BookmarksController < ApplicationController
   def index
   end
 
   def create
+    @bookmark = Bookmark.new bookmark_params
+
+    respond_to do | wants |
+      if @bookmark.save
+        wants.json do
+          render json: { data: { bookmark: @bookmark }, message: 'created' }, status: :created
+        end
+      else
+        wants.json do
+          render json: { data: { errors: @bookmark.errors.full_messages }, message: '' }, status: 422
+        end
+      end
+    end
   end
 
 private
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,3 @@
 class Bookmark < ApplicationRecord
+  validates :email, :image_id, presence: true
 end
