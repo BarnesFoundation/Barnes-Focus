eb55b0715dab911ea727c0421f32bb53b46112c7
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-04-19 13:43:42 +0530

    Automatically trigger input click when coming back from not-found or search results page by clicking take photo.

--- app/javascript/snap/components/SnapNotFound.js
+++ app/javascript/snap/components/SnapNotFound.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router'
 import { Link } from 'react-router-dom';
 
-const SnapNotFound = (props) => (
-    <div className="search-error-container">
-        <div className="error-container">
-            <h1>Try again! We couldn't match that work of art.</h1>
-            <p>Tip: Our snap app is most successful at recognizing two-dimensional works of art like paintings.</p>
-        </div >
-        <div className="row">
-            <div className="col-6 offset-3 col-md-2 offset-md-5 text-center">
-                <Link className="btn snap-btn snap-btn-default" to="/snap">
-                    Take photo
-                </Link>
+import { isIOS, isAndroid, isSafari, isFirefox, isChrome } from 'react-device-detect';
+
+/** 
+ * withRouter HOC provides props with location, history and match objects
+*/
+class SnapNotFound extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick = () => {
+        if(isAndroid && isChrome) {
+            this.props.history.push({
+                pathname: '/',
+                state: {
+                    launchCamera: true
+                }
+            });
+        } else if (isIOS || (isAndroid && isFirefox)) {
+            this.props.history.push({
+                pathname: '/',
+                state: {
+                    launchCamera: true
+                }
+            });
+        }
+    } 
+
+    render() {
+        return (
+            <div className="search-error-container">
+                <div className="error-container">
+                    <h1>Try again! We couldn't match that work of art.</h1>
+                    <p>Tip: Our snap app is most successful at recognizing two-dimensional works of art like paintings.</p>
+                </div >
+                <div className="row">
+                    <div className="col-6 offset-3 col-md-2 offset-md-5 text-center">
+                        <button className="btn snap-btn snap-btn-default" to="/snap" onClick={this.handleClick}>
+                            Take photo
+                        </button>
+                    </div>
+                </div>
+
             </div>
-        </div>
+        );
+    }
+}
 
-    </div>
-);
-
-export default SnapNotFound;
+export default withRouter(SnapNotFound);
