e9b2f2a2e31d36d1f7796ee110022933d89b45cb
Puneet Pandey <er.puneetpandey@gmail.com>
2018-05-03 12:39:18 +0530

    Removing duplicate force_ssl from prodcution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,6 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.force_ssl = true
-  
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
