2539506a5c623b7182340af24126c89e93a2ce14
Puneet Pandey <er.puneetpandey@gmail.com>
2018-05-16 20:26:10 +0530

    branch cut for heroku deployments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@barnesfoundation.org', password: 'b@rne$', password_confirmation: 'b@rne$') if Rails.env.development?
+AdminUser.create!(email: 'puneet@happyfuncorp.com', password: 'b@rne$', password_confirmation: 'b@rne$') if Rails.env.development?
