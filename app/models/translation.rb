afce87cac197410b75e4e7d6194b6b325f6e2170
Puneet Pandey <er.puneetpandey@gmail.com>
2018-05-30 21:01:21 +0530

    translation

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,4 +1,7 @@
 class Translation < ApplicationRecord
+  validates :screen_text, presence: true
+  before_save :set_english_translation
+
   scope :all_parents, -> { where(parent_id: nil) }
   scope :all_childrens, ->(parent_id) { where(parent_id: parent_id) }
 
@@ -12,8 +15,8 @@ class Translation < ApplicationRecord
         all_childrens( main_text.id ).each do | content |
           all_contents = {}
           all_contents[ content.unique_identifier ] = {
-            screen_text: content.screen_text,
-            translated_content: content.send(column_name)
+            screen_text: format(content.screen_text),
+            translated_content: content.send(column_name).blank? ? format(content.english_translation) : format(content.send(column_name))
           }
           translations[ main_text.screen_text ] << all_contents
         end
@@ -36,5 +39,15 @@ class Translation < ApplicationRecord
       }
       return h[language]
     end
+
+    def format text
+      text.gsub("\t", '')
+    end
+  end
+
+  protected
+
+  def set_english_translation
+    self.english_translation = screen_text if english_translation.nil? || english_translation.blank?
   end
 end
