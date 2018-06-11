82b50ee1f00f670e750d2294147b11bf59e1c361
Puneet Pandey <er.puneetpandey@gmail.com>
2018-06-11 17:31:30 +0530

    SV-90

--- app/mailers/bookmark_notifier_mailer.rb
+++ app/mailers/bookmark_notifier_mailer.rb
@@ -1,6 +1,7 @@
 class BookmarkNotifierMailer < ApplicationMailer
-  def send_activity_email email, els_arr_of_objs
+  def send_activity_email email, els_arr_of_objs, lang
     @els_arr = els_arr_of_objs
+    @translations = Translation.find_by_header_text_and_language 'Email', lang
     mail(to: email, subject: 'Thank you for visiting the Barnes today!')
   end
 end
