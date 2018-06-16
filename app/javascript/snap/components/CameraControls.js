ffee28cc908a414cccf4681d51b36a0d7e6b93d6
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-06-16 12:49:01 +0530

    Added transaltions for camera controls.

--- app/javascript/snap/components/CameraControls.js
+++ app/javascript/snap/components/CameraControls.js
@@ -1,35 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { SNAP_LANGUAGE_TRANSLATION } from './Constants';
 
-const CameraControls = (props) => (
+class CameraControls extends Component {
 
-    <div>
-        {
-            !props.searchInProgress &&
-            <div className="camera-controls" >
-                <div className="controls">
-                    {
-                        props.showVideo &&
-                        <div>
-                            <div id="snap-cancel" className="control-left" onClick={props.cancelCamera}>Cancel</div>
-                            <div id="snap-click" className="camera-shutter">
-                                <span>PHOTO</span>
-                                <div className="round-button-circle" onClick={props.takePhoto}></div>
-                            </div>
-                        </div>
-                    }
-
-                    {
-                        !props.showVideo &&
-                        <div>
-                            <div id="snap-retake" className="control-left" onClick={props.clearPhoto}>Retake</div>
-                            <div id="snap-submit" className="control-right" onClick={props.submitPhoto}>Use Photo</div>
-                        </div>
-                    }
-                </div>
-            </div >
+    constructor(props) {
+        super(props);
+        let translationObj = localStorage.getItem(SNAP_LANGUAGE_TRANSLATION);
+        this.state = {
+            translation: (translationObj) ? JSON.parse(translationObj) : null
         }
-    </div>
-);
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    !this.props.searchInProgress &&
+                    <div className="camera-controls" >
+                        <div className="controls">
+                            {
+                                this.props.showVideo &&
+                                <div>
+                                    <div id="snap-cancel" className="control-left" onClick={this.props.cancelCamera}>
+                                        {(this.state.translation) ? this.state.translation.Camera.text_2.translated_content : `Cancel`}
+                                    </div>
+                                    <div id="snap-click" className="camera-shutter">
+                                        <span>{(this.state.translation) ? this.state.translation.Camera.text_1.translated_content : `PHOTO`}</span>
+                                        <div className="round-button-circle" onClick={this.props.takePhoto}></div>
+                                    </div>
+                                </div>
+                            }
+
+                            {
+                                !this.props.showVideo &&
+                                <div>
+                                    <div id="snap-retake" className="control-left" onClick={this.props.clearPhoto}>
+                                        {(this.state.translation) ? this.state.translation.Camera.text_3.translated_content : `Retake`}
+                                    </div>
+                                    <div id="snap-submit" className="control-right" onClick={this.props.submitPhoto}>
+                                        {(this.state.translation) ? this.state.translation.Camera.text_4.translated_content : `Use Photo`}
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    </div >
+                }
+            </div>
+        )
+    }
+
+}
 
 export default CameraControls;
