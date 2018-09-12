63caf2ed0fd29a780468ff80cee981e1a9426db0
Christopher Jativa <cjativa@barnesfoundation.org>
2018-09-12 14:41:06 -0400

    modified the batch size for update_es_cache to update the same number of records as available in the cache

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -28,7 +28,7 @@ class BackgroundJobsController < ApplicationController
   end
 
   def update_es_cache
-    EsCachedRecord.find_in_batches(batch_size: 500) do | es_records |
+    EsCachedRecord.find_in_batches(batch_size: EsCachedRecord.count) do | es_records |
       es_records.each { |es_record|
         es_record.es_data = EsCachedRecord.connect_with_es_endpoint(es_record.image_id)
         es_record.last_es_fetched_at = DateTime.now
