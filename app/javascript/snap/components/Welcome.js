bfac340227d0bba1fb34a6e07a6b8a61a71d85c4
Christopher Jativa <cjativa@barnesfoundation.org>
2018-09-21 10:55:23 -0400

    copied changes from master

--- app/javascript/snap/components/Welcome.js
+++ app/javascript/snap/components/Welcome.js
@@ -67,12 +67,12 @@ class WelcomeComponent extends Component {
 
         // navigator.mediaDevices.getUserMedia() is only supported on iOS > 11.0 and only on Safari (not Chrome, Firefox, etc.)
         if (isIOS && (osVersion >= 11.0)) {
-            return <ReactModal isOpen={true} className="Modal">
-                <div className="browser-modal">
-                    {!isSafari ||
+            if (!isSafari) {
+                return <ReactModal isOpen={true} className="Modal">
+                    <div className="browser-modal">
                         <div>
                             <p className="safari-text">Please use Safari while we work on compatibility with other browsers.</p>
-                            <p className="safari-text">Copy the Snap website address and head to Safari to open it</p>
+                            <p className="safari-text">Copy the address and open it in Safari</p>
                             <button onClick={this.copyUrlToClipboard}>
                                 <span className="safari-link">Tap to copy the website address</span>
                                 <input type="text" value="https://snap.barnesfoundation.org" id="link-text" style={{
@@ -80,10 +80,10 @@ class WelcomeComponent extends Component {
                                     left: '-999em'
                                 }} readOnly={false} contentEditable={true} />
                             </button>
-                        </div>}
-                    }
+                        </div>
                 </div>
-            </ReactModal>
+                </ReactModal>
+            }
         }
 
         // If they're not on iOS 11, it doesn't matter what browser they're using, navigator.mediaDevices.getUserMedia() will return undefined
