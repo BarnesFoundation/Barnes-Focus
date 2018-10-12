2567e6b423cf59e20ed8c2fb1304c62026951443
Christopher Jativa <cjativa@barnesfoundation.org>
2018-10-12 10:50:48 -0400

    defined route for new search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :snaps, only: [ :show ] do
       collection do
         post 'search'
+        post 'searcher'
+        get 'submissionId'
         get 'languages'
       end
     end
