b1846347493271904576a705010af4ef64dc196c
Christopher Jativa <cjativa@barnesfoundation.org>
2018-11-08 14:53:00 -0500

    fixed deletion of the temp file

--- app/controllers/api/snaps_controller.rb
+++ app/controllers/api/snaps_controller.rb
@@ -5,88 +5,6 @@ require 'json'
 class Api::SnapsController < Api::BaseController
   include ActionView::Helpers::SanitizeHelper
 
-  def search    
-    data = params[:image]
-    start_time = Time.now
-    searched_result = { success: false }
-    #using test image for API testing
-
-    api = CudaSift.new
-    image_data = Base64.decode64(data['data:image/jpeg;base64,'.length .. -1])
-    file_name = "#{Rails.root.join('tmp') }/#{SecureRandom.hex}.png"
-    File.open(file_name, 'wb') do |f|
-      f.write image_data
-    end
-
-    file = api.loadFileData(file_name)
-    response = api.search_image(file)
-
-    if response["type"] == CudaSift::RESPONSE_CODES[:SEARCH_RESULTS] && response["results"].any? && is_response_accepted?(response["results"].first)
-      response["image_ids"] = [response["results"].collect{|k| File.basename(k.keys[0], ".jpg").split('_')[0]}.compact[0]].compact
-    else
-      response["image_ids"] = []
-    end
-
-    searched_result = process_searched_images_response(response)
-
-    File.delete(file.path)
-    overall_processing = Time.at((Time.now - start_time).to_i.abs).utc.strftime "%H:%M:%S"
-    searched_result['total_time_consumed']['overall_processing'] = overall_processing if searched_result['total_time_consumed'].present?
-    r = SnapSearchResult.create(
-      searched_image_data: data,
-      api_response: response.except("total_time_consumed"),
-      es_response: searched_result["data"],
-      response_time: searched_result['total_time_consumed']
-    )
-    ImageUploadJob.perform_later(r.id)
-
-    render json: searched_result
-  end
-
-  ## Endpoint for searching the CUDA server and returning an image id
-  def searchCuda
-
-    # Get parameters from the request
-    image = params[:image]
-
-    # Prepare for search
-    start_time = Time.now
-    api = CudaSift.new
-
-    # Get the image data 
-    image_data = Base64.decode64(image['data:image/jpeg;base64,'.length .. -1])
-
-    # Generate temporary file for it
-    file_name = "#{Rails.root.join('tmp') }/#{SecureRandom.hex}.png"
-    File.open(file_name, 'wb') do |f|
-      f.write image_data
-    end
-
-    # Load the file to  begin the CUDA search and delete it once complete
-    file = api.loadFileData(file_name)
-    cuda_response = api.search_image(file)
-    File.delete(file.path)
-
-    image_ids = nil
-
-    # Get the image ids of the match
-    if cuda_response["type"] == CudaSift::RESPONSE_CODES[:SEARCH_RESULTS] && cuda_response["results"].any? && is_response_accepted?(cuda_response["results"].first)
-      image_ids = [cuda_response["results"].collect{|k| File.basename(k.keys[0], ".jpg").split('_')[0]}.compact[0]].compact
-    else
-      image_ids = []
-    end
-    
-    response = { :results => [] }
-
-    if image_ids.length > 0
-      response[:results] << { :item => { :name => image_ids[0] } }
-    else 
-      response[:results] = []
-    end
-
-    render json: response
-  end
-
   def languages
     translator = GoogleTranslate.new preferred_language
     render json: translator.supported_languages(preferred_language)
@@ -118,9 +36,16 @@ class Api::SnapsController < Api::BaseController
       response_time: search_time
     )
 
-    ImageUploadJob.perform_later(result.id, query_image_path)
+    # Get the data data uri
+    query_image_uri = 'data:image/jpeg;base64,' + Base64.encode64(open(query_image_path) { |io| io.read })
 
-    render json: 'Image was stored' 
+    if File.exists?(query_image_path)
+      File.delete(query_image_path)
+    end
+
+    ImageUploadJob.perform_later(result.id, query_image_uri)
+
+    render json: 'Image was stored'
   end
 
   ###### Mark all subsequent methods as private methods ######
@@ -151,74 +76,22 @@ class Api::SnapsController < Api::BaseController
       # Request the image information from Elastic Search
       image_info = EsCachedRecord.search(image_id)
 
-      # Translate the short description if needed
-      if image_info[:shortDescription] && preferred_language.present? 
-        image_info[:shortDescription] = translate_text(image_info[:shortDescription]) 
+      # If short description exists
+      if image_info[:shortDescription]
+
+        # Remove html tags
+        image_info[:shortDescription] = strip_tags(image_info[:shortDescription]).html_safe
+
+        # Translate if needed
+        if preferred_language.present? 
+          image_info[:shortDescription] = translate_text(image_info[:shortDescription]) 
+        end
       end
       return image_info
     end
 
-    def process_searched_images_response searched_images
-      response = { }
-      response["data"] = {"records" => [], "message" => "No records found"}
-      response["success"] = false
-      response["api_data"] = []
-      response["total_time_consumed"] = searched_images["total_time_consumed"]
-      case searched_images["type"]
-        when CudaSift::RESPONSE_CODES[:SEARCH_RESULTS]
-          response["success"] = true
-          get_similar_images(searched_images["image_ids"], response)
-        when CudaSift::RESPONSE_CODES[:IMAGE_NOT_DECODED]
-          response["data"]["message"] = "Invalid image data"
-        when CudaSift::RESPONSE_CODES[:IMAGE_SIZE_TOO_BIG]
-          response["data"]["message"] = "Image size is too big"
-        when CudaSift::RESPONSE_CODES[:IMAGE_SIZE_TOO_SMALL]
-          response["data"]["message"] = "Image size is too small"
-      end
-      response
-    end
-
-    def get_similar_images image_ids, response
-      if image_ids.present?
-        response["data"]["message"] = 'Result Found'
-        image_ids.uniq!
-        image_ids.each do |img|
-          start_time = Time.now
-
-          searched_data = EsCachedRecord.search(img)
-          if preferred_language.present?
-            translator = GoogleTranslate.new preferred_language
-            begin
-              if searched_data["shortDescription"]
-                doc = Nokogiri::HTML(translator.translate(searched_data["shortDescription"]))
-                doc.remove_namespaces!
-                searched_data['shortDescription'] = doc.xpath("//p")[0].content
-                searched_data['shortDescription'] = translator.translate(strip_tags(searched_data["shortDescription"]).html_safe) if searched_data['shortDescription'].nil?
-              end
-            rescue Exception => err
-              p err
-              searched_data['shortDescription'] = strip_tags(searched_data["shortDescription"]).html_safe if searched_data["shortDescription"]
-            end
-          end
-
-          time_diff = Time.now - start_time
-          response['total_time_consumed']['es_endpoint'] = Time.at(time_diff.to_i.abs).utc.strftime "%H:%M:%S"
-          response["data"]["records"] << searched_data
-        end
-      end
-      response
-    end
-
-
     def preferred_language
       params["language"]
-    end
-
-
-    def is_response_accepted? first_response
-      resp_val = nil
-      first_response.select { |k, v| resp_val = v }
-      return resp_val >= ENV.fetch('RESPONSE_THRESHOLD').to_f
     end
 
     ## Translates the given text
