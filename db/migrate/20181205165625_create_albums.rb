2e68c33cac0a629e90800ef5a759048de4499081
Nupur <Nupur@Nupurs-MacBook-Pro.local>
2018-12-06 12:54:32 +0530

    SV-102

--- db/migrate/20181205165625_create_albums.rb
+++ db/migrate/20181205165625_create_albums.rb
@@ -2,7 +2,9 @@ class CreateAlbums < ActiveRecord::Migration[5.1]
   def change
     create_table :albums do |t|
       t.string :name, null: false, default: ""
+      t.string :unique_identifier, null: false
       t.timestamps
     end
+    add_index :albums, :unique_identifier
   end
 end
