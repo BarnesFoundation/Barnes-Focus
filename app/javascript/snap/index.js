a636fee17871a89aac594a0054f7686a002f2b30
Rajnikant Gupta <capricorn.raj@gmail.com>
2018-12-10 09:48:15 +0530

    slick carousel setup.

--- app/javascript/snap/index.js
+++ app/javascript/snap/index.js
@@ -6,6 +6,8 @@ import AppRouter from './routes/AppRouter';
 
 import 'bootstrap/scss/bootstrap.scss'
 import 'font-awesome/scss/font-awesome.scss';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
 import './styles/styles.scss';
 
 
