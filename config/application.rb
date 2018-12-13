895a8a693d1151b6f14e17c01f82c5bfef863e41
Nupur <Nupur@Nupurs-MacBook-Pro.local>
2018-12-13 18:58:51 +0530

    SV-107

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module BarnesSnap
     #config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
     #config.active_job.queue_adapter = :delayed_job
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
 
     Aws.config.update({
       region: Rails.application.secrets[:aws][:region],
