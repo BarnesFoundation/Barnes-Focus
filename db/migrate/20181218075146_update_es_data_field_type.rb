2272a37b8fbd90ed1beb172898c379eb2645cbd6
Puneet Pandey <er.puneetpandey@gmail.com>
2018-12-18 16:00:59 +0530

    SV-109

---
+++ db/migrate/20181218075146_update_es_data_field_type.rb
@@ -0,0 +1,6 @@
+class UpdateEsDataFieldType < ActiveRecord::Migration[5.1]
+  reversible do |change|
+    change.up   { change_column :es_cached_records, :es_data, 'json USING CAST(es_cached_records.es_data AS json)' }
+    change.down { change_column :es_cached_records, :es_data, :text }
+  end
+end
