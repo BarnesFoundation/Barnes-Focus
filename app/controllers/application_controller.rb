5aa9a2f505a28ea448afc1e269558bbd79cc9746
Puneet Pandey <er.puneetpandey@gmail.com>
2019-01-02 15:56:32 +0530

    SV-116 Changing approach from cookie to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+private
+  def generate_session
+    # ideally we should not use reset_session for capturing scanned history. If we do, we may lost all the scanned history captured by the user before
+    # Just placing it here so we know how to handle reset_session for future cases
+    # reset_session
+    return true if session[ :user_scanned_history ]
+    session[ :user_scanned_history ] = JSON.generate []
+    return true
+  end
 end
