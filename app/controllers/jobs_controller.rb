5ff97a5045b5064f16896cfce9420a3c9d5dc992
Puneet Pandey <er.puneetpandey@gmail.com>
2019-01-14 18:51:50 +0530

    SV-127

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -37,41 +37,51 @@ class JobsController < ApplicationController
     head :ok, content_type: "text/html"
   end
 
-  def compose_bookmarks
-    bookmarks = Bookmark.recent_bookmarks.ready_to_deliver
-
-    if bookmarks.any?
-      bookmarks.group_by(&:email).each do | mail, bukmarks |
-        language = bukmarks.last.language
-        language = language || 'en'
-        els_arr = Array.new
-        bukmarks.each { | obj |
-          els_obj = BarnesElasticSearch.instance.get_object(obj.image_id)
-
-          next if els_obj.nil?
-          els_arr.push els_obj
-        }
-
-        begin
-          unless mail.blank?
-            BookmarkNotifierMailer.send_activity_email(mail, els_arr, language).deliver_now
-            bukmarks.each {|b| b.update_attributes(mail_sent: true)}
-          end
-        rescue Exception => ex
-          p "Unable to send email due to #{ex.to_s}"
-        end
-      end
-    end
-
-    head :ok, content_type: "text/html"
-  end
-
   def clear_sessions
     system `bin/rake db:sessions:trim`
     head :ok, content_type: "text/html"
   end
 
-  private
+  def send_bookmarks_email
+    bookmarks = Bookmark.where("email IS NOT NULL").ready_to_deliver.order('created_at DESC')
+
+    if bookmarks.any?
+      bookmarks.group_by(&:email).each do | mail, bukmarks |
+        next if mail.blank?
+
+        latest_bookmark_entry = bukmarks.first
+        time_in_seconds       = ENV['LATEST_BOOKMARK_ENTRY_THRESHOLD'].present? ? ENV['LATEST_BOOKMARK_ENTRY_THRESHOLD'].to_i : 10800
+
+        if latest_bookmark_entry.created_at.utc < time_in_seconds.seconds.ago.utc
+          language = latest_bookmark_entry.language
+          language = language || 'en'
+          els_arr = Array.new
+
+          bukmarks.each { | obj |
+            els_obj = BarnesElasticSearch.instance.get_object(obj.image_id)
+
+            next if els_obj.nil?
+            els_arr.push els_obj
+          }
+
+          begin
+            unless mail.blank?
+              BookmarkNotifierMailer.send_activity_email(mail, els_arr, language).deliver_now
+              bukmarks.each {|b| b.update_attributes(mail_sent: true)}
+            end
+          rescue Exception => ex
+            p "Unable to send email due to #{ex.to_s}"
+          end
+        end
+      end
+
+      p 'done sending bookmark emails'
+    end
+
+    head :ok, content_type: "text/html"
+  end
+
+private
   def set_request_headers
     @request_headers = {
       'HTTP_X_AWS_SQSD_MSGID': request.headers['HTTP_X_AWS_SQSD_MSGID'], 
