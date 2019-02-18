8ae85b00e9e846562df638676963497221001cbf
Puneet Pandey <er.puneetpandey@gmail.com>
2019-02-18 16:38:38 +0530

    fix dump SQL to return correct records for failed scans

--- app/models/album.rb
+++ app/models/album.rb
@@ -13,24 +13,8 @@ class Album < ApplicationRecord
         where(id: ids)
     end
 
-    def self.failed_scans
-      find_by_sql("
-        SELECT * FROM (
-          SELECT
-            albums.*,
-            CAST(
-              SUM(
-                CASE WHEN photos.result_image_url IS NOT NULL THEN 1 ELSE 0 END
-              ) AS INT
-            ) AS successful_match
-            FROM
-              photos
-          INNER JOIN albums ON albums.id = photos.album_id
-          WHERE photos.response_time NOT LIKE 'null'
-          GROUP BY albums.id
-          ORDER BY albums.id DESC
-        ) Q1 WHERE successful_match = 0
-      ")
+    def self.failed_scans # to use in scanned sessions
+      all.pluck(:id) - succeed.pluck(:album_id)
     end
 
 protected
