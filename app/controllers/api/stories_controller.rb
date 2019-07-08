e6d0dc7a6049cd6001ea3ab3f98babdaf1ca9451
Puneet Pandey <Nupur@Nupurs-MacBook-Pro.local>
2019-07-09 01:48:36 +0530

    SV-188, SV-193

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -2,20 +2,37 @@ class Api::StoriesController < Api::BaseController
   skip_before_action :verify_authenticity_token
 
   def show
-    puts params[:slug]
-    @story = StoryFetcher.new.find_by_object_id(5204, 'en')
+    @story      = Story.find_by slug: params[:slug]
 
     respond_to do |wants|
-      wants.json do
-        render json: {
-          data: {
-            success: true,
-            total: @story[:total],
-            unique_identifier: @story[:unique_identifier],
-            content: @story[:content]
+      if @story
+        @story_data = StoryFetcher.new.find_by_title(@story.title, 'en')
+
+        wants.json do
+          render json: {
+            data: {
+              success: true,
+              total: @story_data[:total],
+              unique_identifier: @story_data[:unique_identifier],
+              content: @story_data[:content]
+            },
+            message: 'ok'
           },
-          message: 'ok'
-        }
+          status: :ok
+        end
+      else
+        wants.json do
+          render json: {
+            data: {
+              success: false,
+              total: 0,
+              unique_identifier: nil,
+              content: {}
+            },
+            message: 'Story not found!'
+          },
+          status: 404
+        end
       end
     end
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "barnes-snap_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.asset_host = ENV['ASSET_HOST']
+  config.action_mailer.default_url_options = { :host => ENV['ASSET_HOST'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
