f6cd6962f6ba1a19a67212120f77db83761cf766
Rajnikant Gupta <capricorn.raj@gmail.com>
2019-07-24 20:51:59 +0530

    toggle paragraph to use based on env variable

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,24 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-const webpack = require('webpack')
+const webpack = require('webpack');
 
-environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     jquery: 'jquery',
-    'window.Tether': "tether",
+    'window.Tether': 'tether',
     Popper: ['popper.js', 'default']
-}),
-    new webpack.EnvironmentPlugin([
-        'IMAGE_ENGINE',
-        'CROP_IMAGE',
-        'CATCHOOM_ACCESS_TOKEN',
-        'CATCHOOM_REQUEST_URL',
-        'APP_VERSION'
-    ]) // this makes sure that these are accessible as env variables to react
-)
+  }),
+  new webpack.EnvironmentPlugin([
+    'IMAGE_ENGINE',
+    'CROP_IMAGE',
+    'CATCHOOM_ACCESS_TOKEN',
+    'CATCHOOM_REQUEST_URL',
+    'APP_VERSION',
+    'STORY_PARAGRAPH_TO_USE'
+  ]) // this makes sure that these are accessible as env variables to react
+);
 
-
-
-module.exports = environment
+module.exports = environment;
