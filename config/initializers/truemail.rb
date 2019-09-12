ec11798cf630575cbb1d0716f3a8bf6a4f14aa31
Christopher Jativa <cjativa@barnesfoundation.org>
2019-09-12 10:38:24 -0400

    installed Truemail gem and configured with test email address

---
+++ config/initializers/truemail.rb
@@ -0,0 +1,12 @@
+require 'truemail'
+
+Truemail.configure do |config|
+
+  # Required parameter. Must be an existing email on behalf of which verification will be performed
+  config.verifier_email = 'cjativa@barnesfoundation.org'
+
+  # Optional parameter. This option will be parse bodies of SMTP errors. It will be helpful
+  # if SMTP server does not return an exact answer that the email does not exist
+  # By default this option is disabled, available for SMTP validation only.
+  config.smtp_safe_check = true
+end
