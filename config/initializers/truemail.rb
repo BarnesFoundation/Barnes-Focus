79a3d9bd247fd20588fb671b17bab694385295e2
Christopher Jativa <cjativa@barnesfoundation.org>
2019-09-13 15:09:12 -0400

    turned off smtp checking

--- config/initializers/truemail.rb
+++ config/initializers/truemail.rb
@@ -5,8 +5,10 @@ Truemail.configure do |config|
   # Required parameter. Must be an existing email on behalf of which verification will be performed
   config.verifier_email = 'cjativa@barnesfoundation.org'
 
+  config.default_validation_type = :mx
+
   # Optional parameter. This option will be parse bodies of SMTP errors. It will be helpful
   # if SMTP server does not return an exact answer that the email does not exist
   # By default this option is disabled, available for SMTP validation only.
-  config.smtp_safe_check = true
+  # config.smtp_safe_check = true
 end
