3abb709a5688831c6c54a1089c70b971e882f3bf
Christopher Jativa <cjativa@barnesfoundation.org>
2019-09-17 14:31:57 -0400

    completed automatic language detection and setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 private
+
+  @@supported_languages = [ 'en', 'es', 'fr', 'de', 'it', 'ru', 'zh', 'ja', 'ko' ]
+
   def generate_session
     # ideally we should not use reset_session for capturing scanned history. If we do, we may lost all the scanned history captured by the user before
     # Just placing it here so we know how to handle reset_session for future cases
@@ -12,25 +15,43 @@ private
 	# Return true if the session already exists
 	return true if session[ :user_scanned_history ]
 	
-	puts "No session existed so creating one"
 
 	# Otherwise continue to generate it
 	session[ :user_scanned_history ] = JSON.generate []
 	
+	# Since this block is entered when a session needs to be generated, we set the language preference here
+	set_language_preference()
+	
+    return true
+  end
+
+  ### Utilizes the HTTP accept-language header to automatically detect and set the language for the session
+  def set_language_preference
 	# Locate the session since it's now been generated
 	session = ActiveRecord::SessionStore::Session.find_by_session_id( request.session_options[:id] )
 	
 	if session
 
-		# Modern browsers order the languages by preference, so the first element would be the most preferred
 		accept_language_header = request.headers.fetch('HTTP_ACCEPT_LANGUAGE')
-		preferred_language = HTTP::Accept::Languages.parse(accept_language_header)[0]
 
-		puts "The preferred language to be stored in the session is #{preferred_language.locale}"		
-	else
-		puts "Couldn't retrieve the session"
+		# Modern browsers order the languages by preference, so the first element would be the most preferred. We then strip the dash if it's there
+		pl = HTTP::Accept::Languages.parse(accept_language_header)[0].locale
+
+		# Check if the language is hyphenated 
+		if pl.index('-')
+
+			# Remove it as we only support base language codes
+			pl = pl.slice(0..(pl.index('-') - 1))
+		end
+
+		language_is_supported = @@supported_languages.include? pl
+
+		# If we do support the language, set it
+		if language_is_supported
+			session.update_column(:lang_pref, pl)
+		end
 	end
-	
-    return true
+
   end
+
 end
