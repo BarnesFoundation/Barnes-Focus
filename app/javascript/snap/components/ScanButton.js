8fad87cd2ab32adfe8b62e0069fed2d5b6f2b47e
Christopher Jativa <cjativa@barnesfoundation.org>
2019-10-25 12:57:35 -0400

    fixed aria labelling

--- app/javascript/snap/components/ScanButton.js
+++ app/javascript/snap/components/ScanButton.js
@@ -17,8 +17,8 @@ class ScanButton extends Component {
 
 		return (
 			<div className="scan-wrapper">
-				<div className={`scan-button ${(float) ? 'floating' : ''}`} onClick={handleScan} role="button" aria-roledescription="camera button" >
-					<img src={scan_button} alt="scan" />
+				<div id="camera-btn" className={`scan-button ${(float) ? 'floating' : ''}`} onClick={handleScan} role="button" aria-roledescription="camera button" >
+					<img src={scan_button} alt="scan" aria-labelledby="camera-btn" />
 				</div>
 			</div>
 		)
