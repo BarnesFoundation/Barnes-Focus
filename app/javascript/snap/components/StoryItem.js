102da03a407afdd84c3e4d5824739fa9b4d7f691
Christopher Jativa <cjativa@barnesfoundation.org>
2019-10-25 10:45:51 -0400

    commenting

--- app/javascript/snap/components/StoryItem.js
+++ app/javascript/snap/components/StoryItem.js
@@ -89,8 +89,8 @@ class StoryItem extends React.Component {
 						.fromTo(this.scrollCtaRef, 1, { opacity: 1 }, { opacity: 0 }, 0)
 						.fromTo(this.contentRef, 0.1, { autoAlpha: 0, y: '50px' }, { autoAlpha: 1, y: '0px' }, '-=0.1')
 						.fromTo(this.contentRef, 1.0, { y: '0px' }, { y: -scrollOffset, ease: Linear.easeNone }, '-=0.1');
-				} 
-				
+				}
+
 				// Handling for normal story component
 				else {
 					this.t2
@@ -98,15 +98,15 @@ class StoryItem extends React.Component {
 						.fromTo(this.contentRef, 0.15, { autoAlpha: 0.01, y: '50px' }, { autoAlpha: 1, y: '0px' }, 0)
 						.fromTo(this.contentRef, 1.0, { y: '0px' }, { y: -scrollOffset, ease: Linear.easeNone }, '-=0.16');
 				}
-			} 
-			
+			}
+
 			// Handling for the last story in the list
 			else if (this.props.isLastStoryItem) {
 				this.t2
 					.fromTo(this.contentRef, 0.15, { autoAlpha: 0.01, y: '50px' }, { autoAlpha: 1, y: '0px' })
 					.fromTo(this.contentRef, 1.0, { y: '0px' }, { y: -scrollOffset, ease: Linear.easeNone }, '-=0.16');
-			} 
-			
+			}
+
 			// Handling for all other stories in the list
 			else {
 				this.t2
@@ -165,117 +165,106 @@ class StoryItem extends React.Component {
 		return this.props.story.detail.people.toLowerCase().includes('unidentified');
 	}
 
-  render() {
-	const { story, storyTitle, progress, storyIndex } = this.props;
-	const paragraphFontStyle = localStorage.getItem(SNAP_LANGUAGE_PREFERENCE) === 'Ru' ? { fontSize: `16px` } : {};
+	/** Generates the aria-label for the background image */
+	generateImageAriaLabel = () => {
+		const { title, people, culture } = this.props.story.detail;
+		return `${title} by ${people}${this.isUnidentifiedArtist() ? `, ${culture}` : ''} used as a background image`;
+	}
 
-	// Determines appropriate offset of the story card peeking. 67 + 56 = 123 (address bar height compensation for Android)
-    const peekOffsetStyle =
-      (isAndroid && storyIndex === 0)
-        ? { transform: 'translate3d(0px, -123px, 0px)' }
-        : { transform: 'translate3d(0px, -67px, 0px)' };
-    
-    return (
-      <div>
-        <Timeline totalProgress={progress} paused ref={ref => (this.masterTimeline = ref)}>
-          <div className="card story-item" style={peekOffsetStyle}>
-            {this.props.storyIndex === 0 && this.state.showTitle && !this.props.storyEmailPage && (
-              <div className="story-title-bar">
+	render() {
+		const { story, storyTitle, progress, storyIndex, storyEmailPage, onVisChange } = this.props;
+		const { showTitle } = this.state;
 
-                <div className="story-title">{storyTitle}</div>
-              </div>
-            )}
-            <Timeline
-              totalProgress={progress * 5}
-              paused
-              target={
-                <img className="card-img-top" src={this.getArtUrl()} alt="story_item" style={{ width: `100%` }} role="img" aria-label={`${story.detail.title} by ${story.detail.people}${this.isUnidentifiedArtist() ? `, ${story.detail.culture}` : ''} used as a background image`} />
-              }
-              ref={ref => (this.imgTopTimeline = ref)}>
-              {
-                <Tween
-                  from={{ css: { borderRadius: '24px 24px 0px 0px' } }}
-                  to={{ css: { borderRadius: '0px 0px 0px 0px' } }}
-                  ease="easeOut"
-                  duration={0.2}
-                />
-              }
-            </Timeline>
+		const paragraphFontStyle = localStorage.getItem(SNAP_LANGUAGE_PREFERENCE) === 'Ru' ? { fontSize: `16px` } : {};
 
-            <Timeline
-              totalProgress={progress * 5}
-              paused
-              target={<div className="overlay" />}
-              ref={ref => (this.overlayTimeline = ref)}>
-              <Tween
-                from={{ autoAlpha: 0, borderRadius: '24px 24px 0px 0px' }}
-                to={{ autoAlpha: 0.6, borderRadius: '0px 0px 0px 0px' }}
-                ease="easeOut"
-                duration={0.1}
-              />
-            </Timeline>
+		// Determines appropriate offset of the story card peeking. 67 + 56 = 123 (address bar height compensation for Android)
+		const peekOffsetStyle = (isAndroid && storyIndex === 0) ? { transform: 'translate3d(0px, -123px, 0px)' } : { transform: 'translate3d(0px, -67px, 0px)' };
 
-            {this.props.storyIndex === 0 && <div className="overlay" ref={this.refOverlayCallback} />}
-            <div className="content-mask">
-              <div className="card-img-overlay">
-                {this.props.storyEmailPage && this.props.storyIndex === 0 && (
-                  <div>
-                    <div className="intro" ref={this.refTitleCallback}>
-                      <div className="barnes-logo">
-                        <img src={barnes_logo} alt="barnes_logo" />
-                        <div className="story-name">{storyTitle}</div>
-                      </div>
-                    </div>
-                    <div className="scroll-cta" ref={this.refScrollTextCallback}>
-                      <div className="scroll-icon">
-                        <img src={scroll_up} alt="scroll" />
-                      </div>
-                      <div className="text">{`Scroll to Begin`}</div>
-                    </div>
-                  </div>
-				)}
-                <div className="scroll-text" ref={this.refContentCallback}>
-				  {!this.props.storyEmailPage && <div className="story-name" id={`story-here-${storyIndex}`}>{storyTitle}</div>}
-				  <VizSensor onChange={(isVisible) => { this.props.onVisChange(isVisible, storyIndex)}}>
-                  <div
-                    className="story-text" id={`story-text-${this.props.storyIndex}`}
-                    style={paragraphFontStyle}
-                    dangerouslySetInnerHTML={{
-                      __html:
-                        process.env.STORY_PARAGRAPH_TO_USE === 'long'
-                          ? story.long_paragraph.html
-                          : story.short_paragraph.html
-                    }}
-                  />
-				  </VizSensor>
-                  <div
-                    className="story-footer"
-                    style={{
-                      paddingBottom: this.props.selectedLanguage.code === LANGUAGE_EN ? `200px` : 0
-                    }}>
-                    {story.detail.title}, {story.detail.displayDate}
-                    <br />
-                    {story.detail.people}
-                    {this.isUnidentifiedArtist()
-                      ? ''
-                      : ` (${story.detail.nationality}, ${story.detail.birthDate} - ${story.detail.deathDate})`}
-					{this.isUnidentifiedArtist() ? `, ${story.detail.culture}` : ''}
-                  </div>
-                  {this.props.selectedLanguage.code !== LANGUAGE_EN && (
-                    <div className="google-translate-disclaimer" style={{ paddingBottom: `200px` }}>
-                      <span>Translated with </span>
-                      <img src={google_logo} alt="google_logo" />
-                    </div>
-				  )}
-                </div>
-              </div>
-			</div>	
-          </div>
-		 
-        </Timeline>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Timeline totalProgress={progress} paused ref={ref => (this.masterTimeline = ref)}>
+					<div className="card story-item" style={peekOffsetStyle}>
+
+						{/** Only show the story title for the first card */}
+						{storyIndex === 0 && showTitle && !storyEmailPage && (
+							<div className="story-title-bar">
+								<div className="story-title">{storyTitle}</div>
+							</div>
+						)}
+
+						{/** Displays the background image of the story item */}
+						<Timeline totalProgress={progress * 5} paused ref={ref => (this.imgTopTimeline = ref)}
+							target={<img className="card-img-top" src={this.getArtUrl()} alt="story_item" style={{ width: `100%` }} role="img" aria-label={this.generateImageAriaLabel()} />} >
+							{<Tween from={{ css: { borderRadius: '24px 24px 0px 0px' } }} to={{ css: { borderRadius: '0px 0px 0px 0px' } }} ease="easeOut" duration={0.2} />}
+						</Timeline>
+
+						{/** Displays the overlay */}
+						<Timeline totalProgress={progress * 5} paused target={<div className="overlay" />} ref={ref => (this.overlayTimeline = ref)}>
+							<Tween from={{ autoAlpha: 0, borderRadius: '24px 24px 0px 0px' }} to={{ autoAlpha: 0.6, borderRadius: '0px 0px 0px 0px' }} ease="easeOut" duration={0.1} />
+						</Timeline>
+
+						{this.props.storyIndex === 0 && <div className="overlay" ref={this.refOverlayCallback} />}
+
+						<div className="content-mask">
+							<div className="card-img-overlay">
+
+								{/** In the email page, for the first story item */}
+								{storyEmailPage && storyIndex === 0 && (
+									<div>
+										<div className="intro" ref={this.refTitleCallback}>
+											<div className="barnes-logo">
+												<img src={barnes_logo} alt="barnes_logo" />
+												<div className="story-name">{storyTitle}</div>
+											</div>
+										</div>
+										<div className="scroll-cta" ref={this.refScrollTextCallback}>
+											<div className="scroll-icon">
+												<img src={scroll_up} alt="scroll" />
+											</div>
+											<div className="text">{`Scroll to Begin`}</div>
+										</div>
+									</div>
+								)}
+
+								{/** Scroll text section */}
+								<div className="scroll-text" ref={this.refContentCallback}>
+
+									{/** Display the story title for non-story email pages */}
+									{!storyEmailPage && <div className="story-name" id={`story-here-${storyIndex}`}>{storyTitle}</div>}
+
+									{/** Captures when the story paragraph is in view to notify the parent component */}
+									<VizSensor onChange={(isVisible) => { onVisChange(isVisible, storyIndex) }}>
+										<div className="story-text" id={`story-text-${storyIndex}`} style={paragraphFontStyle}
+											dangerouslySetInnerHTML={{ __html: process.env.STORY_PARAGRAPH_TO_USE === 'long' ? story.long_paragraph.html : story.short_paragraph.html }} />
+									</VizSensor>
+
+									{/** Story footer section */}
+									<div className="story-footer" style={{ paddingBottom: this.props.selectedLanguage.code === LANGUAGE_EN ? `200px` : 0 }}>
+										{story.detail.title}, {story.detail.displayDate}
+										<br />
+										{story.detail.people}
+
+										{/** Display additional information about the artist */}
+										{this.isUnidentifiedArtist() ? '' : ` (${story.detail.nationality}, ${story.detail.birthDate} - ${story.detail.deathDate})`}
+										{this.isUnidentifiedArtist() ? `, ${story.detail.culture}` : ''}
+									</div>
+
+									{/** Display the translation disclaimer */}
+									{this.props.selectedLanguage.code !== LANGUAGE_EN && (
+										<div className="google-translate-disclaimer" style={{ paddingBottom: `200px` }}>
+											<span>Translated with </span>
+											<img src={google_logo} alt="google_logo" />
+										</div>
+									)}
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</Timeline>
+			</div>
+		);
+	}
 }
 
 export default StoryItem;
