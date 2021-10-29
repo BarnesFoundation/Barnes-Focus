8ee1c7cda8a55646e047b7cb9d2694dc34f8badc
Christopher Jativa <cjativa@barnesfoundation.org>
2021-10-28 23:02:07 -0400

    convert to bash file

---
+++ .platform/hooks/prebuild/00_yarn.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Starting yarn steps..."; mkdir -p /home/my_random_dir_1
+curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+yum -y install nodejs
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
+
+echo "Installing packages"
+yum -y install yarn;
+
+echo $PWD; yarn; echo $(ls)
+chown webapp:webapp .bundle/config; mkdir -p /home/webapp
+chown webapp:webapp /home/webapp; chmod 700 /home/webapp; echo $(ls /var/app/staging)
