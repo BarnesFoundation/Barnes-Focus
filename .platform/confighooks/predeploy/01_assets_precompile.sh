2a46a55bb3bd687a6e8531c7d354d7f5163ac348
Christopher Jativa <cjativa@barnesfoundation.org>
2021-11-03 23:10:44 -0400

    debugging commands

--- .platform/confighooks/predeploy/01_assets_precompile.sh
+++ .platform/confighooks/predeploy/01_assets_precompile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Running assets:precompile"
 export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+echo $(ls)
+yarn --production
 cd /var/app/staging; bundle exec rake assets:precompile
 chown -R webapp:webapp /var/app/staging/
