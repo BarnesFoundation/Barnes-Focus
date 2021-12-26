bc61f698ed004774bd1cde472e84064d3d9af6e3
Christopher Jativa <cjativa@barnesfoundation.org>
2021-12-26 00:01:32 -0500

    uncommented permissions work

--- .platform/hooks/prebuild/00_permissions.sh
+++ .platform/hooks/prebuild/00_permissions.sh
@@ -2,11 +2,11 @@
 echo "Bundle install and yarn install"
 su webapp -c "yarn --production";
 
-# echo "Performing permissions work"
-# chown -R webapp:webapp .bundle; 
-# chmod -R 777 .bundle;
-# chown -R webapp:webapp vendor; 
-# chmod -R 777 vendor;
+echo "Performing permissions work"
+chown -R webapp:webapp .bundle; 
+chmod -R 777 .bundle;
+chown -R webapp:webapp vendor; 
+chmod -R 777 vendor;
 
 echo "Permission for home/webapp"
 mkdir -p /home/webapp
