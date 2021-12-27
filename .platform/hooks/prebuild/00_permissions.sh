eca216caa55af5e5cca23ab2de96ac25ed7ac477
Christopher Jativa <cjativa@barnesfoundation.org>
2021-12-27 00:56:59 -0500

    change config permissions

--- .platform/hooks/prebuild/00_permissions.sh
+++ .platform/hooks/prebuild/00_permissions.sh
@@ -3,9 +3,9 @@ echo "Bundle install and yarn install"
 su webapp -c "yarn --production";
 
 echo "Performing permissions work"
-# chown -R webapp:webapp .bundle; 
+chown -R webapp:webapp .bundle; 
 chmod -R 777 .bundle;
-# chown -R webapp:webapp vendor; 
+chown -R webapp:webapp vendor; 
 chmod -R 777 vendor;
 
 echo "Permission for home/webapp"
