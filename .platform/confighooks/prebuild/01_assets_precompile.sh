4155a06249df5e064eb5dce6f03def9fd5aae761
Christopher Jativa <cjativa@barnesfoundation.org>
2022-01-10 00:30:56 -0500

    remove work done in precompile

--- .platform/confighooks/prebuild/01_assets_precompile.sh
+++ .platform/confighooks/prebuild/01_assets_precompile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
 
-cd /var/app/staging 
-su webapp -c "yarn --production";
+# cd /var/app/staging 
+# su webapp -c "yarn --production";
 
-su webapp -c "bundle install";
-bundle exec rake assets:precompile
+# bundle install
+# bundle exec rake assets:precompile
 # chown -R webapp:webapp /var/app/staging/
