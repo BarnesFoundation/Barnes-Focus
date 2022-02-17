fd218bfdbf98aab59b114677d9d7a6b78722a04b
Christopher Jativa <cjativa@barnesfoundation.org>
2022-02-16 20:35:02 -0500

    clean up files

--- .platform/hooks/prebuild/00_permissions.sh
+++ .platform/hooks/prebuild/00_permissions.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 echo "Bundle install and yarn install"
-# su webapp -c "yarn --production";
-
-echo "Performing permissions work"
-# chown -R webapp:webapp .bundle; 
-chmod -R 777 .bundle;
-# chown -R webapp:webapp vendor; 
-chmod -R 777 vendor;
+su webapp -c "yarn --production";
 
 echo "Permission for home/webapp"
 mkdir -p /home/webapp
 chmod 700 /home/webapp;
-
-
-# echo "Making environment variables available"
-# export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
